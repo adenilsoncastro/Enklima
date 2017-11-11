51130235a0bb98bfad51f996fb776c79417a5cf2
oluis <o.luisfernando@outlook.com>
2017-11-11 18:55:29 -0200

    aaa

--- app/src/main/java/com/example/usuario/enklima/MainActivity.java
+++ app/src/main/java/com/example/usuario/enklima/MainActivity.java
@@ -2,12 +2,26 @@ package com.example.usuario.enklima;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.EditText;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.example.usuario.enklima.Model.User;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final String wsUrl = "http://35.167.241.68:3000";
+    private final String wsLoginUrl = wsUrl + "/users/login";
+
 
     private EditText login;
     private EditText password;
@@ -23,12 +37,41 @@ public class MainActivity extends AppCompatActivity {
         password = (EditText)findViewById(R.id.txtPassword);
 
         queue = Volley.newRequestQueue(this);
-
     }
 
-    void login(){
+    public void login(View v){
 
+        final User user = new User();
 
+        user.setLogin(login.getText().toString());
+        user.setPassword(password.getText().toString());
+
+        StringRequest postRequest = new StringRequest(Request.Method.POST, wsLoginUrl,
+                new Response.Listener<String>()
+                {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.d("Response", response);
+                    }
+                },
+                new Response.ErrorListener()
+                {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("Error.Response", error.toString());
+                    }
+                }
+        ) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String>  params = new HashMap<String, String>();
+                params.put("login", user.getLogin());
+                params.put("password", user.getPassword());
+
+                return params;
+            }
+        };
+        queue.add(postRequest);
 
     }
 }
---
+++ app/src/main/java/com/example/usuario/enklima/Model/User.java
@@ -0,0 +1,36 @@
+package com.example.usuario.enklima.Model;
+
+/**
+ * Created by oluis on 11/11/2017.
+ */
+
+public class User {
+
+    private String id;
+    private String login;
+    private String password;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
